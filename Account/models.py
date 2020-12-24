39987c3ccc8d2d467fbd92c76be7502520aad3ca
äniketsonu <56171163+aniketsonu@users.noreply.github.com>
2020-12-25 01:00:51 +0530

    Basic User Authentication set up. Register/ Authenticate/ Login

---
+++ Account/models.py
@@ -0,0 +1,73 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
+
+# Create your models here.
+
+
+class MyAccountManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+        if not first_name:
+            raise ValueError('Users must have a first_name')
+        if not last_name:
+            raise ValueError('Users must have a last_name')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, first_name, last_name, password):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class Account(AbstractUser):
+    email = models.EmailField(verbose_name='email', max_length=60, unique=True)
+    username = None
+    date_joined = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
+    last_login = models.DateTimeField(verbose_name='last login', auto_now=True)
+    first_name = models.CharField(verbose_name='first_name',max_length=50, unique=False)
+    last_name = models.CharField(verbose_name='last_name',max_length=50, unique=False)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+    objects = MyAccountManager()
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return True
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(Account, on_delete=models.CASCADE)
+    about = models.CharField(max_length=100, default='')
+    city = models.CharField(max_length=100, default='')
+    dob = models.DateField(verbose_name='D.O.B', unique=False)
+
+    def __str__(self):
+        return self.user.email
---
+++ Account/views.py
@@ -0,0 +1,70 @@
+from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import render, redirect
+# Create your views here.
+from .forms import AccountAuthenticationForm, RegistrationForm, ProfileForm
+from .models import Account, Profile
+
+
+def login_view(request):
+    context = {}
+    user = request.user
+    if user.is_authenticated:
+        return redirect("dashboard")
+
+    if request.POST:
+        form = AccountAuthenticationForm(request.POST)
+        if form.is_valid():
+            email = request.POST['email']
+            password = request.POST['password']
+            user = authenticate(email=email, password=password)
+
+            if user:
+                login(request, user)
+                return redirect("dashboard")
+
+    else:
+        form = AccountAuthenticationForm()
+        context['login_form'] = form
+
+    return render(request, "login.html", context)
+
+
+def registration_view(request):
+    context = {}
+    if request.POST:
+        form = RegistrationForm(request.POST)
+        profile_form = ProfileForm(request.POST)
+        if form.is_valid() and profile_form.is_valid():
+            user = form.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+            email = form.cleaned_data.get('email')
+            raw_password = form.cleaned_data.get('password1')
+            account = authenticate(email=email, password=raw_password)
+            login(request, account)
+            return redirect("dashboard")
+        else:
+            context['registration_form'] = form
+            context['profile_form'] = profile_form
+            return render(request, 'register.html', context)
+
+    else:
+        form = RegistrationForm()
+        profile_form = ProfileForm(request.POST)
+        context['registration_form'] = form
+        context['profile_form'] = profile_form
+    return render(request, 'register.html', context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
+
+
+def dashboard(request):
+    context = {}
+    user = request.user
+    if user.is_authenticated:
+        return render(request, "dashboard.html", context)
+        
